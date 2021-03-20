bc9386378126e82fbc8059882b32af244b7b75f2
Alif Bin Saif (Binnash) <logicalloard@gmail.com>
2021-03-21 00:44:38 +0600

    test update

--- dist/MyClient.js
+++ dist/MyClient.js
@@ -18,7 +18,7 @@ class MyClient extends Client {
             var _a;
             (_a = client.user) === null || _a === void 0 ? void 0 : _a.setActivity({
                 name: 'Created By Binnash',
-                type: ''
+                
             });
         })
             .catch(console.error);
